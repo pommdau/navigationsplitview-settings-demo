19dcce418a463fa667f4e807934615e16027d9e2
IKEH1024 <pondau1024@gmail.com>
2023-08-01 18:52:54 +0900

    create project

---
+++ NavigationSplitViewSettingsDemo/NavigationSplitViewSettingsDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NavigationSplitViewSettingsDemo
+//
+//  Created by HIROKI IKEUCHI on 2023/08/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationSplitViewSettingsDemo/NavigationSplitViewSettingsDemo/NavigationSplitViewSettingsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSplitViewSettingsDemoApp.swift
+//  NavigationSplitViewSettingsDemo
+//
+//  Created by HIROKI IKEUCHI on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSplitViewSettingsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
