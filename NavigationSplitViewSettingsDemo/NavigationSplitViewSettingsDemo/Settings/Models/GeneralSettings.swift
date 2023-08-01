1c5ad6347a98e94dfb616661059afff8a8308d1d
IKEH1024 <pondau1024@gmail.com>
2023-08-01 21:29:28 +0900

    wip

--- NavigationSplitViewSettingsDemo/NavigationSplitViewSettingsDemo/Settings/Models/GeneralSettings.swift
+++ NavigationSplitViewSettingsDemo/NavigationSplitViewSettingsDemo/Settings/Models/GeneralSettings.swift
@@ -7,25 +7,65 @@
 
 import SwiftUI
 
+extension GeneralSettings {
+    
+    private static var className: String {
+        String(describing: self)
+    }
+    
+    enum UserDefaultsKey: String, CaseIterable {
+        case shapeColor
+        case shapeSize
+        case needsShapeShadow
+        
+        // e.g. "GeneralSettings-shapeSize"
+        var key: String {
+            "\(className)-\(rawValue)"
+        }
+    }
+}
+
+extension GeneralSettings {
+        
+    static func setDefaults() {
+        UserDefaults.standard.register(defaults: [
+            UserDefaultsKey.shapeSize.key: 44,
+        ])
+    }
+    
+    static func resetUserDefaults() {
+        UserDefaultsKey.allCases.forEach { userDefaultsKey in
+            UserDefaults.standard.removeObject(forKey: userDefaultsKey.key)
+        }
+        setDefaults()
+    }
+}
+
 class GeneralSettings: ObservableObject {
     
     let settingsPaneType: SettingsPaneType = .general
 
-    @Published var shapeColor: Color = UserDefaults.standard.color(forKey: "shape-color") ?? .green {
+    @Published var shapeColor: Color = UserDefaults.standard.color(forKey: UserDefaultsKey.shapeColor.key) ?? .green {
         didSet {
-            UserDefaults.standard.set(shapeColor, forKey: "shape-color")
+            UserDefaults.standard.set(shapeColor, forKey: UserDefaultsKey.shapeColor.key)
         }
     }
 
-    @Published var shapeSize: Float = UserDefaults.standard.float(forKey: "shape-size") {
+    @Published var shapeSize: Float = UserDefaults.standard.float(forKey: UserDefaultsKey.shapeSize.key) {
         didSet {
-            UserDefaults.standard.set(shapeSize, forKey: "shape-size")
+            UserDefaults.standard.set(shapeSize, forKey: UserDefaultsKey.shapeSize.key)
         }
     }
     
-    @Published var needsShapeShadow: Bool = UserDefaults.standard.bool(forKey: "needs-shape-shadow") {
+    @Published var needsShapeShadow: Bool = UserDefaults.standard.bool(forKey: UserDefaultsKey.needsShapeShadow.key) {
         didSet {
-            UserDefaults.standard.set(needsShapeShadow, forKey: "needs-shape-shadow")
+            UserDefaults.standard.set(needsShapeShadow, forKey: UserDefaultsKey.needsShapeShadow.key)
         }
     }
+    
+//    init() {
+//        self.shapeColor = shapeColor
+//        self.shapeSize = shapeSize
+//        self.needsShapeShadow = needsShapeShadow
+//    }
 }
